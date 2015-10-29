6baf67a89cce3b9bd55f02a57eee831625584cfa
Kristian <kristian@dragerengen.no>
2015-10-29 16:00:53 +0100

    Client login

--- src/no/hig/ezludo/client/Client.java
+++ src/no/hig/ezludo/client/Client.java
@@ -1,7 +1,20 @@
 package no.hig.ezludo.client;
 
+import javax.swing.*;
+
 /**
  * Created by Kristian on 29.10.2015.
  */
 public class Client {
+    String username;
+    char[] password;
+
+
+    public Client(String username, char[] password) {
+        this.username = username;
+        this.password = password;
+        System.out.print(username);
+        System.out.print(password[0]);
+    }
+
 }
---
+++ src/no/hig/ezludo/client/Login.java
@@ -0,0 +1,132 @@
+package no.hig.ezludo.client;
+
+import Internationalization.Internationalization;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+/**
+ * Created by Kristian on 29.10.2015.
+ */
+public class Login extends JFrame  {
+   private Internationalization internationalization;
+   private ResourceBundle messages;
+   private Client client;
+   private JPanel panel;
+   private BorderLayout border;
+   private String username;
+   private char[] password;
+
+    public Login() {
+        super("Ez-Ludo");
+        //TODO I18N funker ikke
+        //internationalization = new Internationalization(System.getProperty("user.language"), System.getProperty("user.country"));
+       // messages = internationalization.getLang();
+        panel = new JPanel();
+        border = new BorderLayout();
+        panel.setLayout(null);
+
+
+        JLabel userLabel = new JLabel("loginUsername");
+        userLabel.setBounds(10, 10, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 10, 160, 25);
+        userText.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                username = userText.getText();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                username = userText.getText();
+            }
+
+            public void insertUpdate(DocumentEvent e) {
+                username = userText.getText();
+            }
+        });
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("loginPassword");
+        passwordLabel.setBounds(10, 40, 80, 25);
+
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100,40,160,25);
+        passwordText.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                password = passwordText.getPassword();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                password = passwordText.getPassword();
+            }
+
+            public void insertUpdate(DocumentEvent e) {
+                password = passwordText.getPassword();
+            }
+        });
+            panel.add(passwordText);
+
+            JButton loginButton = new JButton("loginLogin");
+            loginButton.setBounds(10,80,80,25);
+            loginButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed (ActionEvent e){
+                    // TODO
+                    // Sjekk mot server om bruker finnes med rett passord
+                // IF BRUKER FINNES:
+                    client = new Client(username, password);
+                    // ELSE
+                    // REGISTRER
+                }
+            }
+
+            );
+            panel.add(loginButton);
+
+            JButton registerButton = new JButton("loginRegister");
+        registerButton.setBounds(180, 80, 80, 25);
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // REGISTER
+            }
+        });
+        panel.add(registerButton);
+        this.add(panel);
+    }
+
+    public static void main(String[] args) {
+
+        // Look and feel
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        Login login = new Login();
+        login.setPreferredSize(new Dimension(350, 150));
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.pack();
+        login.setVisible(true);
+    }
+}
