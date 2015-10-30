108c5f7ab8e7f62f4ecbf7b70ce60bb68129f15e
Kristian <kristian@dragerengen.no>
2015-10-30 21:02:27 +0100

    User Validation done

--- src/no/hig/ezludo/client/UserAccount.java
+++ src/no/hig/ezludo/client/UserAccount.java
@@ -22,6 +22,8 @@ public class UserAccount {
     private char[] password;
     private char[] passwordRepeat ;
     private String email;
+    private int errorsNumb=0;
+    private String[] errors = new String[20];
 
 
     public UserAccount(Internationalization internationalization) {
@@ -152,22 +154,20 @@ public class UserAccount {
         registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (passwordChecker(password, passwordRepeat)) {
-                    System.out.print(" OK PASSORD");
-                } else
-                    System.out.print(" FEIL PASSORD");
-
-                if (emailChecker(email)) {
-                    System.out.print(" OK MAIL");
+                String output="";
+                for (int i = 0; i<=19; i++) {
+                    errors[i] = null;
                 }
-                else
-                    System.out.print(" FEIL mail");
 
-                if (usernameChecker(username)) {
-                    System.out.print(" OK USERNAME");
+                if (usernameChecker(username) && emailChecker(email) && passwordChecker(password, passwordRepeat)) {
                 }
-                else
-                    System.out.print(" FEIL USERNAME");
+                else {
+                    for (int i=1; i<=errorsNumb; i++) {
+                        if (errors[i] != null)
+                        output=output+"\n"+errors[i];
+                    }
+                    JOptionPane.showMessageDialog(null, output, messages.getString("errormsg"), JOptionPane.WARNING_MESSAGE);
+                }
             }
 
         });
@@ -179,40 +179,70 @@ public class UserAccount {
     public boolean passwordChecker(char[] a, char[] b) {
         int length;
 
-        if (a == null || b == null)
+        if (a == null || b == null) {
+            errorsNumb++;
+            errors[errorsNumb] = messages.getString("registerNoPassword");
             return false;
+        }
 
-        if (a.length != b.length)
+
+        if (a.length != b.length) {
+            errorsNumb++;
+            errors[errorsNumb] = messages.getString("registerPasswordNoMatch");
             return false;
+        }
         else
             length = a.length;
-        if (length < 8)
+        if (length < 8) {
+            errorsNumb++;
+            errors[errorsNumb] = messages.getString("registerPasswordTooShort");
             return false;
+        }
         for (int i = 0; i < length; i++){
-            if (a[i] != b[i])
+            if (a[i] != b[i]) {
+                errorsNumb++;
+                errors[errorsNumb] = messages.getString("registerPasswordNoMatch");
                 return false;
+            }
         }
 
         return true;
     }
 
     public boolean emailChecker(String email) {
-        if (email == null)
+        if (email == null) {
+            errorsNumb++;
+            errors[errorsNumb] = messages.getString("registerNoEmail");
             return false;
-        System.out.print(email);
-        //TODO sjekk om email er satt
+        }
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
         java.util.regex.Matcher m = p.matcher(email);
+        if(!m.matches()) {
+            errorsNumb++;
+            errors[errorsNumb] = messages.getString("registerEmailNotValid");
+        }
         return m.matches();
     }
 
 
     public boolean usernameChecker (String username) {
-        if (username == null)
+        if (username == null) {
+            errorsNumb++;
+            errors[errorsNumb] = messages.getString("registerNoUsername");
             return false;
-        if (username.length() < 3 || username.length() > 15)
+        }
+        if (username.length() < 3 || username.length() > 15) {
+            if (username.length() < 3) {
+                errorsNumb++;
+                errors[errorsNumb] = messages.getString("registerShortUsername");
+            }
+            if (username.length() > 15) {
+                errorsNumb++;
+                errors[errorsNumb] = messages.getString("registerLongUsername");
+            }
             return false;
+        }
         else
             return true;
     }
