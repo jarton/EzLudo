81f193e83bc69ee027bbbd3a49c698c1da49cb4f
Kristian <kristian@dragerengen.no>
2015-11-01 22:22:20 +0100

    ludoboard class

---
+++ src/no/hig/ezludo/client/LudoBoard.java
@@ -0,0 +1,15 @@
+package no.hig.ezludo.client;
+
+import javax.swing.*;
+
+/**
+ * Created by Kristian on 01.11.2015.
+ */
+public class LudoBoard extends JPanel {
+
+    //// MAIN FJERNES
+    public static void main(String[] args) {
+
+    }
+
+}
