f3650176bfae77034ee431a95488b03d874abc74
Per-Kristian <perkristian.nilsen@gmail.com>
2015-11-01 19:32:26 +0100

    Should be able to send chat messages to the server, via the lobby chat. Still won't display messages received (including the chat message sent to the server)

--- src/no/hig/ezludo/client/MainController.java
+++ src/no/hig/ezludo/client/MainController.java
@@ -10,11 +10,27 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-
+/**
+ * This class handles the GUI of the lobby, and whatever events may happen in the window.
+ * Date: 30.10.2015
+ * @author Per-Kristian Nilsen
+ */
 public class MainController extends Application {
     public ListView chatListView;
-
     public Scene lobbyScene;
+    private Client client;
+
+    public MainController(Client clientObject) {
+        client = clientObject;
+        launch(null);
+    }
+
+
+    /**
+     * This method loads the lobby window.
+     * @param primaryStage the JavaFX stage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Lobby.fxml"));
@@ -23,20 +39,29 @@ public class MainController extends Application {
         primaryStage.show();
     }
 
+    /**
+     * This method is called when the user presses enter in the text field. It will send the message to the client
+     * object, which further sends the message to the server.
+     * @param event the triggered event of pressing enter in the text field
+     */
     @FXML
     public void handleTextFieldEvent(ActionEvent event){
         TextField source = (TextField) event.getSource();
-        System.out.println(source.getText());
-        inputTextToChat(source.getText());
-        // TODO: Pass chat text to Client.
+        client.sendChatMessage(source.getText());
     }
 
-    public void inputTextToChat(String text) {
+    /**
+     * This method receives a message, and displays it in the lobby chat.
+     * @param text
+     */
+    public void displayLobbyMessage(String text) {
         chatListView.getItems().add(text);
     }
 
+    /*
     public static void main(String[] args) {
         launch(args);
     }
+    */
 
 }
