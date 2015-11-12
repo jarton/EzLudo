f29273064c6e1ecd1b4487056a91bfa30cf80427
Kristian <kristian@dragerengen.no>
2015-11-12 13:14:39 +0100

    Added Ludo Icon

--- src/no/hig/ezludo/client/GameController.java
+++ src/no/hig/ezludo/client/GameController.java
@@ -12,33 +12,11 @@ import java.util.Random;
  */
 public class GameController {
     @FXML private ImageView dice;
-    private int diceNr;
-    private final int diceMin = 1;
-    private final int diceMax = 6;
-    private final int rounds = 5;
+
 
     public GameController() { }
 
 
-    public int rollDices() {
-        for (int i =0; i<=rounds; i++) {
-            diceNr = randomInt(diceMin, diceMax);
-            // Show Image .. set image in imgview dice+diceNr+.png
-            delay(1000);
-        }
-        return diceNr;
-    }
 
-    public void delay(long time) {
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException ex) {}
-    }
-
-    public static int randomInt(int min, int max) {
-        Random rand = new Random();
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-        return randomNum;
-    }
 
 }
--- src/no/hig/ezludo/server/Game.java
+++ src/no/hig/ezludo/server/Game.java
@@ -1,5 +1,6 @@
 package no.hig.ezludo.server;
 
+import java.util.Random;
 import java.util.Vector;
 
 /**
@@ -9,6 +10,10 @@ public class Game {
     private User players[] = new User[4];
     private String name = "Random Game";
     private int id = -1;
+    private int diceNr;
+    private final int diceMin = 1;
+    private final int diceMax = 6;
+    private final int rounds = 5;
 
     public Game(User players[]) {
         this.players = players;
@@ -20,5 +25,26 @@ public class Game {
 
     public String getName() {
         return name;
+    }
+
+    public int rollDices() {
+        for (int i =0; i<=rounds; i++) {
+            diceNr = randomInt(diceMin, diceMax);
+            // Show Image .. set image in imgview dice+diceNr+.png
+            delay(1000);
+        }
+        return diceNr;
+    }
+
+    public void delay(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException ex) {}
+    }
+
+    public static int randomInt(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
     }
 }
