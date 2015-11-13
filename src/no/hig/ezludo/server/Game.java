a2a8579b74a1048aa8aa57e6ca7a7aeeb9e655ad
Kristian <kristian@dragerengen.no>
2015-11-13 12:36:13 +0100

    Added LudoBoard Coordinates Updated LudoBoard to find coordinates Removed Dice functionality from Game.java Added Dice func to RollDice.Java (Command)

--- src/no/hig/ezludo/server/Game.java
+++ src/no/hig/ezludo/server/Game.java
@@ -10,10 +10,6 @@ public class Game {
     private User players[] = new User[4];
     private String name = "Random Game";
     private int id = -1;
-    private int diceNr;
-    private final int diceMin = 1;
-    private final int diceMax = 6;
-    private final int rounds = 5;
 
     public Game(User players[]) {
         this.players = players;
@@ -27,24 +23,4 @@ public class Game {
         return name;
     }
 
-    public int rollDices() {
-        for (int i =0; i<=rounds; i++) {
-            diceNr = randomInt(diceMin, diceMax);
-            // Show Image .. set image in imgview dice+diceNr+.png
-            delay(1000);
-        }
-        return diceNr;
-    }
-
-    public void delay(long time) {
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException ex) {}
-    }
-
-    public static int randomInt(int min, int max) {
-        Random rand = new Random();
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-        return randomNum;
-    }
 }
